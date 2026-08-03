5673bfa7feeb1b5ab81b678f5376a45075430eb4
Sanjaikumar <siosanjai29@gmail.com>
2026-08-03 15:20:29 +0530

    Update app.py

--- src/app.py
+++ src/app.py
@@ -1,7 +1,7 @@
 # Python
 This is for your vscode, juypter notebook terminals by this code you can run on those platforms
 
-pip install ollama (install this,or else you get an error)
+pip install ollama  # install this,or else you get an error
 
 from ollama import chat
 
